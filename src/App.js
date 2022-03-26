c4c840d975b2889b95e152a28c4cb7f364410280
Igor Silvestru <igor.silvestru.work@gmail.com>
2022-03-26 15:55:45 +0100

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,72 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useMemo, useState } from "react";
+import { Route, Routes, useSearchParams } from "react-router-dom";
+import { names } from "./data";
+import styled from "@emotion/styled";
 
+const Names = styled.div`
+  column-count: 7;
+`;
+const Input = styled.input`
+  border: 4px solid indigo;
+  margin: 30px auto;
+  display: block;
+  width: 300px;
+  background-color: seashell;
+  height: 50px;
+  color: black;
+  &::placeholder {
+    color: gray;
+  }
+`;
+const Li = styled.li`
+  list-style-type: none;
+  margin-bottom: 5px;
+  font-size: 18px;
+  font-weight: bold;
+  color: green;
+`;
 function App() {
+  const [data, setData] = useState(() => names);
+  const [searchParams, setSearchParams] = useSearchParams();
+  const [value, setValue] = useState(searchParams.get("name") || "");
+  const filteredNames = useMemo(
+    () =>
+      data.filter((name) => name.toLowerCase().includes(value.toLowerCase())),
+    [data, value]
+  );
+
+  useEffect(() => {
+    setSearchParams({ name: value });
+  }, [value]);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Routes>
+      <Route
+        path="/"
+        element={
+          <div className="App">
+            <div>
+              <form>
+                <Input
+                  value={value}
+                  onChange={(e) => {
+                    setValue(e.target.value);
+                  }}
+                  placeholder="Введите имя"
+                />
+              </form>
+            </div>
+            <Names>
+              <ul>
+                {filteredNames.map((name) => (
+                  <Li key={name}>{name}</Li>
+                ))}
+              </ul>
+            </Names>
+          </div>
+        }
+      />
+    </Routes>
   );
 }
 
